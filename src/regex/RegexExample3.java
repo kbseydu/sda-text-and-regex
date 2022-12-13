bd574bb43e45484b1fc9b25b8004e7c1c7674b1f
kbseydu <konradbojczuk@wp.pl>
2022-12-13 20:44:26 +0100

    Uaktualniono RegexExample3 o komentarz

--- src/regex/RegexExample3.java
+++ src/regex/RegexExample3.java
@@ -7,6 +7,7 @@ public class RegexExample3 {
     public static void main(String[] args) {
         Pattern pattern = Pattern.compile("(.*\\-\\-)(\\w+)(\\-\\-.*)");
         Matcher matcher = pattern.matcher("Francis Junior --Underwood-- został prezydentem USA");
+        //System.out.println(matcher.matches()); - musi być inaczej by się wywalil na tych poniższych linijkach z grupami
         System.out.println(matcher.matches());
         System.out.println(matcher.group(1));
         System.out.println(matcher.group(2));
